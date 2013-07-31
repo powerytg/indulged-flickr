2e0d6672197e8ebe7e0b02abeb5a50e85612dae1
Tiangong You / Toby <powerytg@gmail.com>
2013-07-31 15:20:23 -0700

    Added sharpen and solarize filters

---
+++ Indulged/Indulged/Plugins/ProFX/Filters/FXSharpenFilter.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using Nokia.Graphics.Imaging;
+
+namespace Indulged.Plugins.ProFX.Filters
+{
+    public partial class FXSharpenFilter : FilterBase
+    {
+        private SharpnessLevel level = SharpnessLevel.Level2;
+
+        public FXSharpenFilter()
+        {
+            InitializeComponent();
+
+            DisplayName = "sharpen";
+        }
+
+        protected override void CreateFilter()
+        {
+            Filter = FilterFactory.CreateSharpnessFilter(level);
+        }
+
+        private void AmountSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (AmountSlider == null)
+                return;
+
+            int intValue = (int)AmountSlider.Value;
+
+            if ((SharpnessLevel)intValue != level)
+            {
+                level = (SharpnessLevel)intValue;
+                UpdatePreviewAsync();
+            }
+            
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            DeleteFilterAsync();
+        }
+
+        
+    }
+}
--- Indulged/Indulged/Plugins/ProFX/ImageProcessingPageCoreExtension.cs
+++ Indulged/Indulged/Plugins/ProFX/ImageProcessingPageCoreExtension.cs
@@ -37,6 +37,8 @@ namespace Indulged.Plugins.ProFX
 
         // Available filters
         public static List<FilterBase> AvailableFilters = new List<FilterBase> {
+            new FXSolarizeFilter(),
+            new FXSharpenFilter(),
             new FXSepiaFilter(),
             new FXPosterizeFilter(),
             new FXOilFilter(),
