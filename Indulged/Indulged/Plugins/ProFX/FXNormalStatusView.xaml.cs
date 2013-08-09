32223f454a69767bb3305346da7f0344444643c4
Tiangong You / Toby <powerytg@gmail.com>
2013-08-08 20:50:31 -0700

    Added missing files

---
+++ Indulged/Indulged/Plugins/ProFX/FXNormalStatusView.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace Indulged.Plugins.ProFX
+{
+    public partial class FXNormalStatusView : UserControl
+    {
+        public FXNormalStatusView()
+        {
+            InitializeComponent();
+        }
+
+        private void TitleButton_Click(object sender, RoutedEventArgs e)
+        {
+            ImageProcessingPage.RequestFilterListView(this, null);
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            ImageProcessingPage.RequestFilterListView(this, null);
+        }
+    }
+}
